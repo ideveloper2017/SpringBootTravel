47df99a0ca2f438234c394d40f3865a0c82abe09
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-16 14:16:10 +0500

    commit

---
+++ src/test/java/uz/developer/app/SpingBootTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.developer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
